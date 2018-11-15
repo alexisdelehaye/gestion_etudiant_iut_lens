06228bca82a0a2d621f375f0abd26df2559848d3
Alexis Delehaye <alexis_delehaye@ens.univ-artois.fr>
2018-11-15 17:52:30 +0100

    correction mineure

--- projetS5/database/migrations/2018_10_03_140000_create_etudiants_table.php
+++ projetS5/database/migrations/2018_10_03_140000_create_etudiants_table.php
@@ -22,7 +22,7 @@ class CreateEtudiantsTable extends Migration
             $table->string('numEtu',45);
             $table->string('groupe',5);
             $table->integer('Formation_idFormation')->unsigned();
-          //  $table->integer('Semestre_idSemestre')->unsigned();
+            $table->integer('Semestre_idSemestre')->unsigned();
 
             $table->timestamps();
 
@@ -31,7 +31,7 @@ class CreateEtudiantsTable extends Migration
         //Schema::enableForeignKeyConstraints();
         Schema::table('etudiants', function($table) {
            $table->foreign('Formation_idFormation')->references('idFormation')->on('formations');
-          //  $table->foreign('Semestre_idSemestre')->references('idSemestre')->on('semestres');
+            $table->foreign('Semestre_idSemestre')->references('idSemestre')->on('semestres');
 
         });
 
